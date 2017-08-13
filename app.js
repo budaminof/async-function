aac71fb3df1a8ad88bfb5236ffd1e4e06c1e8e1b
bud aminof <budaminof@gmail.com>
2017-08-13 15:06:13 -0600

    async function

---
+++ app.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+
+let numCalls = 0;
+
+// Get user input and store in json file.
+//     Json is read by dfp.js script.
+// Execute dfp.js
+
+// const limit = parseInt(process.argv[2]);
+
+if (process.argv[2] === '-h' || process.argv[2] === '--help' || !process.argv[2]) {
+  console.log("help string with instructions and possible flags")
+  process.exit(0);
+}
+
+let args = {};
+for (let i = 1; i < process.argv.length; i++) {
+  let arg = process.argv[i];
+  if (/-\w/.test(arg)) {
+    args[arg.replace("-", "")] = process.argv[i + 1];
+    i++;
+  }
+}
+
+// console.log(args);
+for ( let i = args.s; i < args.e; i++) {
+  console.log(args.o, args.s, args.e);
+}
+
+
+// function getDogStuff(limit) {
+//   return axios.get("https://dog.ceo/api/breeds/list/all").then((res) => {
+//     console.log("resolved " + numCalls);
+//     numCalls++;
+//     if (numCalls < limit) {
+//         return getDogStuff(limit);
+//     }
+//   })
+// }
+//
+// getDogStuff(limit)
---
+++ budScript.js
@@ -0,0 +1,52 @@
+// const { exec } = require('child_process');
+
+
+let num = 0;
+
+// function execScript (limit) {
+//
+//   exec('node ./app.js', (err, stdout, stderr) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(stdout);
+//     num++;
+//     if( num < limit ) {
+//       execScript(limit)
+//     }
+//   });
+//
+// }
+//
+// execScript(5);
+
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function execScriptAsync(numOrders, numScriptRuns, orderName) {
+  //hgjh
+  let startingLocation = 0;
+  let endingLocation = 100;
+  let orderNameLocal;
+  for (let i = 0; i < numOrders; i++) {
+    orderNameLocal = orderName + i;
+    for (let j = 0; j < numScriptRuns; j++) {
+      startingLocation += 100;
+      endingLocation += 100;
+      const { stdout, stderr } = await exec(`node app.js -o ${orderNameLocal} -e ${endingLocation} -s ${startingLocation}`);
+      console.log("Loop ", j)
+      console.log(stdout);
+    }
+  }
+}
+
+
+execScriptAsync(2, 3, "Test")
+// // Script with spaces in the filename:
+// const bat = spawn('"my script.cmd"', ['a', 'b'], { shell: true });
+// // or:
+// exec('"my script.cmd" a b', (err, stdout, stderr) => {
+//   // ...
+// });
